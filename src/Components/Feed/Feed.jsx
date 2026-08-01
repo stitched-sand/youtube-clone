7420750413b7dbf1a334d02061bb2044072835a6
stitched-sand. <sandraservat@gmail.com.>
2026-08-01 16:09:45 -0400

    feat: finalize YouTube clone with responsive design

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -30,7 +30,9 @@ useEffect(() =>{
     <div className="feed">
         {data.map((item, index) =>{
             return (
-                <Link to={`video/${item.snippet.categoryId}/${item.id}`} className='card'>
+                <Link 
+                key={item.id}
+                to={`video/${item.snippet.categoryId}/${item.id}`} className='card'>
       <img src={item.snippet.thumbnails.medium.url} alt="" />
       <h2>{item.snippet.title}</h2>
       <h3>{item.snippet.channelTitle}</h3>
